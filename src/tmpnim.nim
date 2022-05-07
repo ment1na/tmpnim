06d01e4968e4547507b1c5656f3a95b8a17be7e9
ment1na <80897440+ment1na@users.noreply.github.com>
2022-05-07 18:31:10 +0000

    fix
    fuck i forgot to export the symbols

--- src/tmpnim.nim
+++ src/tmpnim.nim
@@ -3,11 +3,11 @@ proc mount(special_file, dir, fstype: cstring, rwflag: culong, data: pointer): c
 proc umount(dir: cstring): cint {.importc, header: "<sys/mount.h>".} #import the magic header that do black magic
 
 
-proc createRamdisk(directory: string, size: int): bool = #PROGRAM MUST BE RUN AS ROOT TO WORK OR ELSE IT WILL FAIL
+proc createRamdisk*(directory: string, size: int): bool = #PROGRAM MUST BE RUN AS ROOT TO WORK OR ELSE IT WILL FAIL
     var sex: string = fmt"size={size},uid=0,gid=0,mode=777" #i'm not going to change the variable name.
     if mount(cstring"tmpfs", cstring(directory), cstring"tmpfs", 0, cstring(sex)) == 0:
         return true #return true if the status code is 0, so it worked correctly
-proc removeRamdisk(directory: string): bool =   #PROGRAM MUST BE RUN AS ROOT TO WORK OR ELSE IT WILL FAIL
+proc removeRamdisk*(directory: string): bool =   #PROGRAM MUST BE RUN AS ROOT TO WORK OR ELSE IT WILL FAIL
     if umount(cstring(directory)) == 0:
         return true #same as createRamdisk
         
